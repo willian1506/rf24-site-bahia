b6abf77933951571bfaf5511a19e06969f237e95
willian1506 <125928553+willian1506@users.noreply.github.com>
2026-03-30 12:40:10 -0300

    Update script.js

--- script.js
+++ script.js
@@ -16,7 +16,8 @@ const db = firebase.database();
 const ADMINS = [
   {user:"willian1506", pass:"willian123", nome:"Willian"},
   {user:"stormy", pass:"183524", nome:"Stormy"},
-  {user:"Mkz", pass:"12456453", nome:"Mkz"}
+  {user:"Mkz", pass:"12456453", nome:"Mkz"},
+  {user:"gusline1", pass:"18376423", nome:"Gusline"}
 ];
 
 let isAdmin = localStorage.getItem("admin") === "true";
